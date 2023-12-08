600df221d9b2fe100e6432e38d8d197fcd129da5
JoarM <joar.maltesson@gmail.com>
2023-12-08 20:15:06 +0100

    fix: redirect logged in user to dashboard

--- app/route.ts
+++ app/route.ts
@@ -1,5 +1,9 @@
+import { isLoggedIn } from "@/data/user";
 import { redirect } from "next/navigation";
 
 export async function GET() {
+    if ((await isLoggedIn())) {
+        return redirect("/dashboard");
+    }
     return redirect("/home");
 }
