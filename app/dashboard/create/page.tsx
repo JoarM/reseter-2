b5ca2ae93a57c1c0424e1d24b1f9d13c73e7f38d
JoarM <joar.maltesson@gmail.com>
2023-12-08 20:15:42 +0100

    style: make create & dashboard page more uniform

--- app/dashboard/create/page.tsx
+++ app/dashboard/create/page.tsx
@@ -3,7 +3,7 @@ import { CreateForm } from "@/components/utils/create-form";
 
 export default function create() {
     return (
-        <div className="w-[1440px] max-w-full mx-auto px-8 mt-12">
+        <div className="w-[1052px] max-w-full mx-auto px-6 mt-12">
             <BackButton 
             fallback="/dashboard"
             />
