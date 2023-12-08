1bc7e7639c054eac06ce12072c15bf7feb3a142d
Joar Maltesson <joar05@hotmail.com>
2023-12-08 16:48:19 +0100

    fix: starting pe modals

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,7 +1,14 @@
+import { Input } from "@/components/ui/input";
+import { Search } from "lucide-react";
+
 export default function Dashboard() {
     return (
-        <>
+        <div className="w-[1052px] max-w-full mx-auto px-6 mt-12">
+            <div className="relative">
+                <Search className="absolute text-muted-foreground left-3 top-3" size={16} />
+                <Input role="search" className="pl-8" placeholder="Search..." />
+            </div>
             
-        </>
+        </div>
     )
 }
