e2b42166a8a0bc65b49daa4ac56a2fa9f2e99968
JoarM <joar.maltesson@gmail.com>
2023-12-15 22:02:18 +0100

    feat: query individual project

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,8 +1,8 @@
+"use server";
+
 import { db } from "@/server/db";
-import { auth } from "@/server/lucia";
-import { project, team_to_project, user_to_project, user_to_team } from "@/server/schema";
-import { or, sql } from "drizzle-orm";
-import * as context from "next/headers";
+import { project as projectDb, team_to_project, user_to_project, user_to_team } from "@/server/schema";
+import { and, eq, or, sql } from "drizzle-orm";
 import { getUnsanitizedUser } from "./user";
 
 export async function getProjects() {
@@ -10,13 +10,13 @@ export async function getProjects() {
     if (!user) return;
 
     const projects = db.select()
-    .from(project)
+    .from(projectDb)
     .where(
         or(
-            sql`${project.id} IN (
+            sql`${projectDb.id} IN (
             SELECT ${user_to_project.project_id} FROM ${user_to_project} WHERE ${user_to_project.user_id} = ${user.userId}
             )`,
-            sql`${project.id} IN (
+            sql`${projectDb.id} IN (
                 SELECT ${team_to_project.project_id} FROM ${team_to_project} WHERE ${team_to_project.team_id} IN (
                     SELECT ${user_to_team.team_id} FROM ${user_to_team} WHERE ${user_to_team.user_id} = ${user.userId}
                 )
@@ -27,6 +27,19 @@ export async function getProjects() {
     return projects;
 }
 
-export function getProject(id: string) {
-    
+export async function getProject(id: string) {
+    const user = await getUnsanitizedUser();
+    if (!user) return;
+
+    const intId = Number(id);
+
+    const project = await db.select()
+    .from(projectDb)
+    .where(
+        and(
+            eq(projectDb.id, intId), 
+            sql`${intId} IN (SELECT ${user_to_project.project_id} FROM ${user_to_project} WHERE ${user_to_project.user_id} = ${user.userId})`
+        )
+    );
+    return project[0];
 }
