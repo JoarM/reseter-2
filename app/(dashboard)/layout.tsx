1d5143df7d524bee6c1c136300a8f664a6c8adf3
Joar Maltesson <joar05@hotmail.com>
2023-12-15 17:16:04 +0100

    fix: removed unused import

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button";
 import { getUser, isLoggedIn } from "@/data/user";
 import Link from "next/link";
 import { redirect } from "next/navigation";
-import Image from "next/image";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default async function DashboardLayout(props: {children: React.ReactNode, modal: React.ReactNode}) {
