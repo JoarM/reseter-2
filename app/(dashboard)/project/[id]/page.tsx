2aecdcc75a329e2b9d619c39f09603a09b61c8b6
Joar Maltesson <joar05@hotmail.com>
2023-12-18 15:47:42 +0100

    feat: Complete usage chart

--- app/(dashboard)/project/[id]/page.tsx
+++ app/(dashboard)/project/[id]/page.tsx
@@ -1,4 +1,5 @@
-import { UsageChart } from "@/components/utils/usage-chart";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import LineChart from "@/components/ui/line-chart";
 import { getProject } from "@/data/projects"
 import { notFound } from "next/navigation";
 
@@ -22,21 +23,40 @@ export default async function Project({
     for (let i = 0; i < rawUsage.length; i++) {
         const date = new Date();
         date.setDate(date.getDate() - i);
-        const formatedDate = date.toLocaleDateString("en-us", {
+        const formatedDate = date.toLocaleDateString("en-uk", {
+            month: "short",
+            day: "numeric",
+        });
+        const longDate = date.toLocaleDateString("en-uk", {
+            year: "numeric",
             month: "short",
             day: "numeric",
         });
         usage.unshift({
             date: formatedDate,
+            tooltip: longDate,
             "usage": rawUsage[i]
         });
     }
 
     return (
         <>
-            <UsageChart 
-            usage={usage}
-            />
+            <Card className="mt-14">
+                <CardHeader>
+                    <CardTitle>Usage</CardTitle>
+                </CardHeader>
+                <CardContent>
+                    <LineChart
+                    data={usage}
+                    categories={["usage"]}
+                    index="date"
+                    tooltipLabelIndex="tooltip"
+                    className='text-sm font-medium h-72'
+                    yAxisWidth={40}
+                    />
+                </CardContent>
+            </Card>
+            
         </>
     )
 }
