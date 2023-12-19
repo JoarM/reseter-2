425e42c04615c346b6a8f5530d6b8aaad5f46ef2
Joar Maltesson <joar05@hotmail.com>
2023-12-19 12:55:27 +0100

    feat: added apikeys

---
+++ app/(dashboard)/project/[id]/api-keys/page.tsx
@@ -0,0 +1,43 @@
+import { Button } from "@/components/ui/button";
+import { getApiKeys } from "@/data/projects"
+import { Plus } from "lucide-react";
+
+export default async function ApiKeys({ 
+    params
+} : { 
+    params: { 
+        id: string 
+    } 
+}) {
+    const apikeys = await getApiKeys(params.id);
+
+    return (
+        <>
+            <div className="px-2 h-16 flex items-center justify-between">
+                <h1 className="text-lg font-medium">Api keys</h1>
+                <Button>
+                    <Plus
+                    className="mr-2"
+                    size={16}
+                    />
+                    New api key
+                </Button>
+            </div>
+
+            <ul>
+                {apikeys && apikeys.length > 0 ? 
+                    <>
+                        {apikeys.map(({name, key}) => 
+                            <li className="px-2 h-16 flex items-center justify-between border-b border-t border-border" key={key}>
+                                <span className="font-medium text-base">{name}</span>
+                                <span>•••••••••••••••</span>
+                            </li>
+                        )}
+                    </>
+                :
+                    <span className="text-sm font-medium px-2 mt-1">No api keys for this project. Create one to get started</span>
+                }
+            </ul>
+        </>
+    )
+}
--- src/data/projects.ts
+++ src/data/projects.ts
@@ -1,7 +1,7 @@
 "use server";
 
 import { db } from "@/server/db";
-import { project as projectDb, team_to_project, user_to_project, user_to_team } from "@/server/schema";
+import { apikey, project as projectDb, team_to_project, user_to_project, user_to_team } from "@/server/schema";
 import { and, eq, or, sql } from "drizzle-orm";
 import { getUnsanitizedUser } from "./user";
 
@@ -38,8 +38,40 @@ export async function getProject(id: string) {
     .where(
         and(
             eq(projectDb.id, intId), 
-            sql`${intId} IN (SELECT ${user_to_project.project_id} FROM ${user_to_project} WHERE ${user_to_project.user_id} = ${user.userId})`
+            or(
+                sql`${intId} IN (SELECT ${user_to_project.project_id} FROM ${user_to_project} WHERE ${user_to_project.user_id} = ${user.userId})`,
+                sql`${intId} IN (
+                    SELECT ${team_to_project.project_id} FROM ${team_to_project} WHERE ${team_to_project.team_id} IN (
+                        SELECT ${user_to_team.team_id} FROM ${user_to_team} WHERE ${user_to_team.user_id} = ${user.userId}
+                    )
+                )`
+            )
         )
     );
     return project[0];
+}
+
+export async function getApiKeys(projectId: string) {
+    const user = await getUnsanitizedUser();
+    if (!user) return;
+
+    const id = Number(projectId);
+
+    const apikeys = await db.select()
+    .from(apikey)
+    .where(
+        and(
+            eq(apikey.project_id, id), 
+            or(
+                sql`${id} IN (SELECT ${user_to_project.project_id} FROM ${user_to_project} WHERE ${user_to_project.user_id} = ${user.userId})`,
+                sql`${id} IN (
+                    SELECT ${team_to_project.project_id} FROM ${team_to_project} WHERE ${team_to_project.team_id} IN (
+                        SELECT ${user_to_team.team_id} FROM ${user_to_team} WHERE ${user_to_team.user_id} = ${user.userId}
+                    )
+                )`
+            )
+        )
+    );
+
+    return apikeys;
 }
